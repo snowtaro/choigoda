7731ae307d25aa0d4d1c9a473d0751c28f5f621e
snowtaro <ask0208ask@gmail.com>
2025-07-21 21:09:52 +0900

    initial commit

---
+++ src/main/java/com/choigoda/choigoda/ChoigodaApplication.java
@@ -0,0 +1,13 @@
+package com.choigoda.choigoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoigodaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoigodaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choigoda/choigoda/ChoigodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choigoda.choigoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoigodaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
